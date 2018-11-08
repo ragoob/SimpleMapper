6017fc5f029b317e6e79f86caa0486f3d7b3f050
Mohammed <regoo707@gmail.com>
2018-11-08 19:10:27 +0200

    Initial  commit

---
+++ Mapper/MapperExtention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+namespace Mapper
+{
+    public static class MapperExtention
+    {
+        public static V Map<T,V>(this T source)
+        {
+            var instance = Activator.CreateInstance<V>();
+            foreach (var item in source.GetType().GetProperties())
+            {
+                if (instance.GetType().GetProperty(item.Name) != null)
+                {
+                    var prop = instance.GetType().GetProperty(item.Name);
+                    prop.SetValue(item.GetValue(source), instance);
+                }
+            }
+            return instance;
+
+        }
+
+        
+    }
+}
